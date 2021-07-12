9bf42490cef8458339d266ec1c3a482b4d51662a
vinay r a <vinay7.net@gmail.com>
2021-07-13 00:01:10 +0530

    Models project complete. Repository project created with Account Repository.

---
+++ BlogLab.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage ="Username is required")]
+        [MinLength(5, ErrorMessage ="Must be 5-20 characters")]
+        [MaxLength(20, ErrorMessage = "Must be 5-20 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        [MinLength(8,ErrorMessage ="Must be 8-50 characters")]
+        [MaxLength(50, ErrorMessage ="Must be 8-50 characters")]
+        public string Password { get; set; }
+    }
+}
